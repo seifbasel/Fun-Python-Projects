05c10aae7dbe7e3326e3fbe0eff395f919e6a746
seif basel <seifbasel950@gmail.com>
2022-11-04 22:50:15 +0200

    enter

--- rainbow task .py
+++ rainbow task .py
@@ -4,12 +4,29 @@ tu=Turtle()
 bgcolor("lightblue")
 tu.speed(0)
 
+# ground
+tu.penup()
+tu.goto(-500,0)
+tu.pendown()
+tu.width(250)
+tu.color("green")
+tu.forward(1000)
+
+# sea ground
+tu.penup()
+tu.goto(-500,-250)
+tu.pendown()
+tu.width(300)
+tu.color("#1a75ff", "#1a75ff")
+tu.forward(1000)
+
+
 # sun
 tu.penup()
-tu.goto(-300,300)
+tu.goto(-360,320)
 tu.pendown()
 tu.color("yellow")
-tu.width(200)
+tu.width(150)
 tu.dot()
 tu.penup()
 tu.home()
@@ -40,13 +57,13 @@ sky(-90,250)
 
 #go to sea start point
 tu.penup()
-tu.backward(470)
+tu.backward(500)
 tu.right(90)
 tu.forward(300)
 # first wave
 tu.pendown()
 for i in range(9):
-	tu.color("blue","blue")
+	tu.color('#005ce6','#005ce6')
 	tu.width(30)
 	tu.circle(50,180,50)
 	tu.right(180)
@@ -55,13 +72,13 @@ for i in range(9):
 #go to sea start point
 tu.penup()
 tu.home()
-tu.backward(550)
+tu.backward(500)
 tu.right(90)
 tu.forward(250)
 tu.pendown()
 # second wave
 for i in range(9):
-	tu.color("blue","blue")
+	tu.color('#005ce6','#005ce6')
 	tu.width(30)
 	tu.circle(50,180,50)
 	tu.right(180)
@@ -72,13 +89,13 @@ for i in range(9):
 #go to sea start point
 tu.penup()
 tu.home()
-tu.backward(470)
+tu.backward(500)
 tu.right(90)
 tu.forward(200)
 tu.pendown()
 # third wave
 for i in range(9):
-	tu.color("blue","blue")
+	tu.color('#005ce6','#005ce6')
 	tu.width(30)
 	tu.circle(50,180,50)
 	tu.right(180)
@@ -88,13 +105,13 @@ for i in range(9):
 #go to sea start point
 tu.penup()
 tu.home()
-tu.backward(550)
+tu.backward(500)
 tu.right(90)
 tu.forward(350)
 tu.pendown()
 # forth wave
 for i in range(9):
-	tu.color("blue","blue")
+	tu.color('#005ce6','#005ce6')
 	tu.width(30)
 	tu.circle(50,180,50)
 	tu.right(180)
@@ -105,12 +122,12 @@ tu.penup()
 tu.home()
 
 
-# move to start point
+# # move rainbow to start point
 tu.penup()
-tu.forward(150)
 tu.right(90)
-tu.forward(130)
+tu.forward(90)
 tu.left(90)
+
 # violet
 tu.forward(200)
 tu.left(90)
@@ -162,7 +179,7 @@ tu.color("red","red")
 tu.circle(320,180,50)
 
 # white
-tu.color("lightblue","lightblue")
+tu.color('green','green')
 tu.right(270)
 tu.forward(640)
 
